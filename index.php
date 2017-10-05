640e5e06980ceed1e580dd2082b953a76e533f74
serby888 <she_ka@mail.ru>
2017-10-05 11:55:46 +0300

    Com6
    + плеер без модального окна
    + телефонные номера смотрятся приятнее

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
 </head>
 <body>
-	<script src="https://code.jquery.com/jquery.min.js"></script>
+	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
 
@@ -21,35 +21,153 @@
 $(document).ready(function() {
     var table = $('#mytable').DataTable( {
 
-        aoColumnDefs: [{orderable: false, aTargets : [0]}], 
-        "order": [1, 'asc'], 
+        aoColumnDefs: [{orderable: false, aTargets : [2]}], 
+        "order": [0, 'desc'], 
         "paging":   false,        
         "info":     false
 
     } );
 
-
-
-	
 	$('#myInput').on( 'keyup', function () {
 	    table
 	        .search( this.value )
 	        .draw();
 	} );
-
-
-
-
 } );
-
-
-
-
-
-
 
 </script>
 
+<?php
+
+		session_start();
+
+    if (isset($_SESSION['pl']) AND $_SESSION['del'] == true)	
+	{
+		unlink($_SESSION['pl']);
+	}
+date_default_timezone_set('Europe/Minsk');
+
+echo '<table id="mytable" class="table table-hover table-bordered table-row text-center ">
+		<thead>
+                     <tr>
+                     <th>Дата звонка</th>
+                     <th>Телефон абонента</th>
+                     <th>Play</th>
+                     </tr>
+                     </thead>';
+
+Files('*.WAV');
+Files('*/*.WAV');
+Files('*/*/*.WAV');
+Files('*/*/*/*.WAV');
+
+echo "</table>"; 
+
+$play = Player();
+
+
+function Player()
+{
+	if(isset($_GET['play']))
+	{
+
+		$fpath = $_GET['play'];
+		$path = explode(".", $_GET['play']);
+		$pathes = "$fpath $path[0]";
+		 
+		exec('E:\Programs\XAMPP\htdocs\ffmpeg-20170918-18821e3-win64-static\bin\ffmpeg.exe -i '.$pathes.'new.wav');
+
+
+		
+		return $path[0].'new.wav';
+	  		/*echo '<div id="parent_popup">
+	          		<div id="popup">
+  					  <div class="panel panel-default">
+    				    <div class="panel-heading">'."Воспроизводится: <b>",$fpath.'<b></div>
+    					<div class="panel-body">
+    					  <form action = "index.php?delete='.$path[0].'" method = "post">
+	                		<p> <input  class="btn btn-default button" type="submit" name="close" value="Закрыть"></p>
+	                		  <p> 
+	                		  	<audio controls autoplay>
+						 	  	  <source src="'.$path[0].'new.wav" type="audio/wav">
+						 	  		Если вы видите это, значит ваш браузер не
+						 	  		поддерживает теги HTML5 аудио.
+								</audio>
+							  </p>
+	                	  </form>
+    					</div>
+  					  </div>
+					</div>
+				  </div>';
+				  echo '    
+				  <nav class="navbar navbar-default navbar-fixed-top">
+				    <div class="container-fluid">
+				  <div class="navbar-form navbar-right">
+    							<audio controls autoplay>
+						 	  	  <source src="'.$path[0].'new.wav" type="audio/wav">
+						 	  		Если вы видите это, значит ваш браузер не
+						 	  		поддерживает теги HTML5 аудио.
+								</audio>    	
+								</div>	
+    </div>
+    </nav>';*/
+
+	}
+}
+
+function Files($dir)
+{
+	foreach (glob($dir) as $filename) 
+	{
+	$path = $filename;
+	$filename = end(explode('/', $filename));
+
+	echo "<tr>";
+    		$datetime = ConvDate($filename);
+        	echo "<td>$datetime</td>"; 
+
+				$number = strtok(".");
+
+				if (strlen($number) == 12)
+				{
+					$number1 = substr($number, -7, 3);
+					$number2 = substr($number, -4, 2);
+					$number3 = substr($number, -2);
+					$code = substr($number, -9, 2);
+					$number = "+ 375 (".$code.") ".$number1."-".$number2."-".$number3;
+					
+				}
+
+				if (strlen($number) == 6)
+				{
+					$number1 = substr($number, -6, 2);
+					$number2 = substr($number, -4, 2);
+					$number3 = substr($number, -2);
+					$number = $number1."-".$number2."-".$number3;
+
+				}
+
+        		echo "<td>$number</td>"; 
+        		
+    echo "<td><a href='index.php?play=".$path."'><span class='glyphicon glyphicon-play'></span></a></td>
+    </tr>";
+	} 
+}
+
+function ConvDate($name)
+{
+	$date = strtok($name, "-"); 
+	$time = strtok("-");
+	$datetime1 = "$date $time";
+	$datetime = date_create($datetime1);
+	$datetime = date_format($datetime, 'Y-m-d H:i:s');
+
+	return $datetime;
+}
+
+
+
+?>
 
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
@@ -77,109 +195,29 @@ $(document).ready(function() {
       </div>
     </form>
 
-</nav> 
+    <div class="navbar-form navbar-right">
+    	<audio controls autoplay>
 
-
-<?php
-
-date_default_timezone_set('Europe/Minsk');
-
-echo '<table id="mytable" class="table table-hover table-bordered table-row text-center ">
-		<thead>
-                     <tr>
-                     <th>Play</th>
-                     <th>Название записи</th>
-                     <th>Дата звонка</th>
-                     <th>Телефон абонента</th>
-                     </tr>
-                     </thead>';
-
-Files('*.WAV');
-Files('*/*.WAV');
-Files('*/*/*.WAV');
-Files('*/*/*/*.WAV');
-
-echo "</table>"; 
-
-Player();
-
-	if (isset($_POST['close']) AND isset($_GET['delete']))
-	{
-		unlink($_GET['delete'].'new.wav');
-		header("location:index.php");
-	}
-
-
-
-
-function Player()
-{
-	if(isset($_GET['play']))
-	{
-		$fpath = $_GET['play'];
-		$path = explode(".", $_GET['play']);
-		$pathes = "$fpath $path[0]";
-
-		exec('E:\Programs\XAMPP\htdocs\ffmpeg-20170918-18821e3-win64-static\bin\ffmpeg.exe -i '.$pathes.'new.wav');
-
-	  		echo '<div id="parent_popup">
-	          		<div id="popup">
-  					  <div class="panel panel-default">
-    				    <div class="panel-heading">'."Воспроизводится: <b>",$fpath.'<b></div>
-    					<div class="panel-body">
-    					  <form action = "index.php?delete='.$path[0].'" method = "post">
-	                		<p> <input  class="btn btn-default button" type="submit" name="close" value="Закрыть"></p>
-	                		  <p> 
-	                		  	<audio controls autoplay>
-						 	  	  <source src="'.$path[0].'new.wav" type="audio/wav">
+						 	  	  <source src="<?php echo $play ?>" type="audio/wav">
 						 	  		Если вы видите это, значит ваш браузер не
 						 	  		поддерживает теги HTML5 аудио.
-								</audio>
-							  </p>
-	                	  </form>
-    					</div>
-  					  </div>
-					</div>
-				  </div>';
-	}
-}
-
-function Files($dir)
-{
-	foreach (glob($dir) as $filename) 
-	{
-	$path = $filename;
-	$filename = end(explode('/', $filename));
-
-	echo "<tr>
-			<td><a href='index.php?play=".$path."'><span class='glyphicon glyphicon-play'></span></a></td>";
-    	echo "<td> $filename </td>"; 
-    		$datetime = ConvDate($filename);
-        	echo "<td>$datetime</td>"; 
-
-				$number = strtok(".");
-        		echo "<td>$number</td>"; 
-        		
-    echo "</tr>";
-	} 
-}
-
-function ConvDate($name)
-{
-	$date = strtok($name, "-"); 
-	$time = strtok("-");
-	$datetime1 = "$date $time";
-	$datetime = date_create($datetime1);
-	$datetime = date_format($datetime, 'Y-m-d H:i:s');
-
-	return $datetime;
-}
 
 
-?>
+		</audio>    		
+    </div>
+
+
+
+</div>
+</nav> 
 
 
 
 
 </body>
 </html>
+
+<?php
+$_SESSION['del'] = true;
+$_SESSION['pl'] = $play;
+?>
